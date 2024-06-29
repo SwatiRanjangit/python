68e0617ac8f08ca3be86a0ba607be88a5a3a00e1
Swati Ranjan <swatirp83@gmail.com>
2024-06-29 13:59:17 +0530

    python

--- numpydemo.py
+++ numpydemo.py
@@ -1,24 +1,25 @@
-import array as a
-####### ARRAY ##################
-b = a.array('i',[10,20,40,30]) # Here 'i' represent typecode means which type of elemenets in the array
-# print(b)
-# print(type(b))
-# print(b.typecode)
-# print(b[3])
+from numpy import *
+# l = array([10,20.0,30,40,50])
+# l = array([10,20.0,30,40,50],dtype=int)
+# print(l)
+# print(l.ndim) # print dimesion of the array
+# print(l.dtype) # print datatype of elements
+# print(l.shape)
 
-## ARRAY OPERATIONS
-# b.insert(1,90) # will insert one number at the defined index
-# print(b)
-# b.append(80) # will add one element at the last of the array
-# print(b)
-# b.remove(30)
-# print(b)
+# print(linspace(2,20,9)) # it will divide 2 to 20 in total 9 parts (start,stop,increment)
+# print(arange(2,20,2)) print the number in range 2 to 20 with increment of 2 values (start,stop,increment)
+# A = zeros(10,int)  will print array with 10 zero default datatype is float
+# print(A)
+# A = ones(10,int)   will print array with 10 one default datatype is float
+# print(A)
 
-# $$$ INTERATE array elements
-# for i in b:
-#     print(i,end=" ")
 
-# for i in range(4):
-#     print(b[i],end=" ")
-for i in range(len(b)):
-    print(b[i],end=" ")
+A = array([[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+          )
+
+print(A)
+print(A.ndim)
+print(A[1][2])
+print(A.shape)
